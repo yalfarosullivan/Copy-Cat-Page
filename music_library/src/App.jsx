c27e47b7d71e34e2a5efdde53bcbc0cb84efe7b5
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2023-01-04 22:58:07 -0800

    corrected gallery item

---
+++ music_library/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import './App.css';
+import Gallery from './Components/Gallery'
+import SearchBar from './Components/SearchBar'
+
+ 
+const App = () => {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
+
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  useEffect(() => {
+      if(search) {
+          const fetchData = async () => {
+              document.title = `${search} music`
+              const response = await fetch(API_URL + search)
+              const resData = await response.json()
+              if (resData.results.length > 0) {
+                  return setData(resData.results)
+              } else {
+                  return setMessage('Not Found.')
+              }
+          }
+          fetchData()
+      }
+  }, [search])
+
+  const handleSearch = (e, term) => {
+      e.preventDefault()
+      setSearch(term)
+  }
+
+  return (
+      <div>
+          <SearchBar handleSearch={handleSearch} />
+          {message}
+          <Gallery data={data} />
+      </div>
+  )
+}
+
+export default App
